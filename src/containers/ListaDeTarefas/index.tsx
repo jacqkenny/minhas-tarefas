555739f6b04d189c07b372e983700d6e42bb177d
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2024-08-23 20:41:03 -0300

    salvando alterações

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -3,14 +3,15 @@ import { useSelector } from 'react-redux'
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
-import { RootReducer } from '../../store'
+import { RootReducer } from '../../store/'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
 
   return (
     <Container>
-      <p> 2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
+      <p> 2 tarefas marcadas como: &quot;categoria&quot; e &quot;{termo}&quot;</p>
       <ul>
         {itens.map((t) => (
           <li key={t.titulo}>
